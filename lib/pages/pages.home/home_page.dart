6883110c76950cba171305e158ec716c72f82830
juanesgor19 <juanesgor@gmail.com>
2022-02-23 09:41:26 -0500

    Final Step

--- lib/pages/pages.home/home_page.dart
+++ lib/pages/pages.home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -6,39 +7,55 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return (Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
         title: const Text('Uber Clone'),
       ),
       body: SafeArea(
-        child: Column(children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Image.asset('assets/images/logo_app.png',
-                  width: 150, height: 100),
-              const Text("Rapido Y Facil")
-            ],
-          ),
-          const SizedBox(height: 50),
-          const Text("SELECCIONA TU ROL"),
-          const SizedBox(height: 50),
-          const CircleAvatar(
-            backgroundImage: AssetImage('assets/images/pasajero.png'),
-            radius: 50,
-            backgroundColor: Colors.black,
-          ),
-          const SizedBox(height: 10),
-          const Text("Cliente"),
-          const SizedBox(height: 20),
-          //Parte Conductor
-          const CircleAvatar(
-            backgroundImage: AssetImage('assets/images/pasajero.png'),
-            radius: 50,
-            backgroundColor: Colors.black,
-          ),
-          const SizedBox(height: 10),
-          const Text("Cliente"),
-        ]),
+        child: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.red, Colors.green])),
+          child: Column(children: [
+            ClipPath(
+              clipper: DiagonalPathClipperTwo(),
+              child: Container(
+                color: Colors.white,
+                height: MediaQuery.of(context).size.height * 0.30,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Image.asset('assets/images/logo_app.png',
+                        width: 150, height: 100),
+                    const Text("Rapido Y Facil")
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 50),
+            const Text("SELECCIONA TU ROL",
+                style: TextStyle(color: Colors.white)),
+            const SizedBox(height: 50),
+            const CircleAvatar(
+              backgroundImage: AssetImage('assets/images/pasajero.png'),
+              radius: 50,
+              backgroundColor: Colors.grey,
+            ),
+            const SizedBox(height: 10),
+            const Text("Cliente", style: TextStyle(color: Colors.white)),
+            const SizedBox(height: 30),
+            //Parte Conductor
+            const CircleAvatar(
+              backgroundImage: AssetImage('assets/images/pasajero.png'),
+              radius: 50,
+              backgroundColor: Colors.grey,
+            ),
+            const SizedBox(height: 10),
+            const Text("Conductor", style: TextStyle(color: Colors.white)),
+          ]),
+        ),
       ),
     ));
   }
